c25896dbdf7dfc7d314684e4ed7353f30beeea72
HortonMike1997 <vulehoangm@gmail.com>
2017-12-26 14:40:24 +0700

    copy source

---
+++ ConnectToMaple/ConnectToMaple/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace ConnectToMaple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.Tinh = new System.Windows.Forms.Button();
+            this.Rich = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btOCR = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(2, 12);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(2, 79);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtOutput.Size = new System.Drawing.Size(82, 55);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.TextChanged += new System.EventHandler(this.txtOutput_TextChanged);
+            // 
+            // Tinh
+            // 
+            this.Tinh.Location = new System.Drawing.Point(2, 50);
+            this.Tinh.Name = "Tinh";
+            this.Tinh.Size = new System.Drawing.Size(75, 23);
+            this.Tinh.TabIndex = 2;
+            this.Tinh.Text = "button1";
+            this.Tinh.UseVisualStyleBackColor = true;
+            this.Tinh.Click += new System.EventHandler(this.Tinh_Click_1);
+            // 
+            // Rich
+            // 
+            this.Rich.Location = new System.Drawing.Point(2, 186);
+            this.Rich.Name = "Rich";
+            this.Rich.Size = new System.Drawing.Size(75, 56);
+            this.Rich.TabIndex = 3;
+            this.Rich.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(377, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(341, 371);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(724, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(190, 371);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btOCR
+            // 
+            this.btOCR.Location = new System.Drawing.Point(13, 269);
+            this.btOCR.Name = "btOCR";
+            this.btOCR.Size = new System.Drawing.Size(75, 23);
+            this.btOCR.TabIndex = 6;
+            this.btOCR.Text = "button1";
+            this.btOCR.UseVisualStyleBackColor = true;
+            this.btOCR.Click += new System.EventHandler(this.btOCR_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.AllowDrop = true;
+            this.txtResult.Location = new System.Drawing.Point(128, 186);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(193, 181);
+            this.txtResult.TabIndex = 7;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 407);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.btOCR);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Rich);
+            this.Controls.Add(this.Tinh);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button Tinh;
+        private System.Windows.Forms.RichTextBox Rich;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btOCR;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ ConnectToMaple/ConnectToMaple/Form1.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using Tesseract;
+using System.Threading.Tasks;
+
+namespace ConnectToMaple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+            //load Maple
+            MapleEngine.MapleCallbacks cb;
+            byte[] err = new byte[2048];
+            IntPtr kv;
+            String[] argv = new String[2];
+            argv[0] = "maple";
+            argv[1] = "-A2";
+            cb.textCallBack = cbText;
+          
+            cb.errorCallBack = cbError;
+            cb.statusCallBack = cbStatus;
+            cb.readlineCallBack = null;
+            cb.redirectCallBack = null;
+            cb.streamCallBack = null;
+            cb.queryInterrupt = null;
+            cb.callbackCallBack = null;
+
+            try
+            {
+                kv = MapleEngine.StartMaple(2, argv, ref cb, IntPtr.Zero, IntPtr.Zero, err);
+            }
+            catch (DllNotFoundException)
+            {
+                return;
+            }
+            catch (EntryPointNotFoundException)
+            {
+                return;
+            }
+
+            if (kv.ToInt64() == 0)
+            {
+                MessageBox.Show("Fatal Error, could not start Maple: "
+                        + System.Text.Encoding.ASCII.GetString(err, 0, Array.IndexOf(err, (byte)0)), "Lỗi", MessageBoxButtons.OK);
+                return;
+            }
+            //Load package mình vừa viết, tên là TEST
+            try
+            {
+                MapleEngine.EvalMapleStatement(kv, Encoding.ASCII.GetBytes("read(\"D:/TAM/TEST2.mpl\");with(TEST);"));
+                // MapleEngine.EvalMapleStatement(kv, Encoding.ASCII.GetBytes("with(TEST):"));
+                Rich.Text = "";
+            }
+            catch (Exception)
+            {
+            }
+        }
+        string str;
+        public void cbText(IntPtr data, int tag, IntPtr output)
+        {
+            //Xuất kết quả ra ngoài
+            str = str + Marshal.PtrToStringAnsi(output) + "\\\\";
+            Rich.Text = Rich.Text + "\n" + Marshal.PtrToStringAnsi(output);
+            
+            
+        }
+
+        public static void cbError(IntPtr data, IntPtr offset, IntPtr msg)
+        {
+            string s = Marshal.PtrToStringAnsi(msg);
+        }
+
+        public static void cbStatus(IntPtr data, IntPtr used, IntPtr alloc, double time)
+        {
+
+        }
+        int bien_a = 0, bien_b = 0, bien_c = 0;
+        
+        private void Tinh_Click_1(object sender, System.EventArgs e)
+        {
+
+            MapleEngine.MapleCallbacks cb;
+            byte[] err = new byte[2048];
+            IntPtr kv;
+
+            String[] argv = new String[2];
+            argv[0] = "maple";
+            argv[1] = "-A2";
+
+            cb.textCallBack = cbText;
+            cb.errorCallBack = cbError;
+            cb.statusCallBack = cbStatus;
+            cb.readlineCallBack = null;
+            cb.redirectCallBack = null;
+            cb.streamCallBack = null;
+            cb.queryInterrupt = null;
+            cb.callbackCallBack = null;
+            Rich.Text = "";
+            str = "";
+
+
+            int a = 2, b = 4, c = 6;
+
+            try
+            {
+
+                kv = MapleEngine.StartMaple(2, argv, ref cb, IntPtr.Zero, IntPtr.Zero, err);
+
+                try
+                {
+                    //sử dụng hàm Tong trong package TEST
+                    String expr = "Tong(";
+
+                    expr += txtInput.Text;
+                    //expr += a + "," + b + "," + c;
+                    expr += ");";
+
+                    IntPtr val = MapleEngine.EvalMapleStatement(kv, Encoding.ASCII.GetBytes(expr));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Không thể load Maple", "Lỗi", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception)
+            {
+            }
+            string tmp = "D:\\TAM\\";
+            // txtOutput.Text = Path.Combine(tmp, "Eq2ImgWinForms.gif");
+            txtOutput.Text = str;
+            NativeMethods.CreateGifFromEq(str, Path.Combine(tmp, "Eq2ImgWinForms.gif"));
+            pictureBox1.Image = Image.FromFile(Path.Combine(tmp, "Eq2ImgWinForms.gif"));
+            pictureBox2.Image = Image.FromFile(Path.Combine(tmp, "plot.jpg"));
+        }
+
+        private void txtOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        //private static MapleEngine.TextCallBack callbackDelegate;
+        private void btOCR_Click(object sender, EventArgs e)
+        {
+             if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                var img = new Bitmap(openFileDialog.FileName);
+                var ocr = new TesseractEngine("./tessdata", "vie", EngineMode.Default);
+                var page = ocr.Process(img);
+                string equation = page.GetText();
+                var builder = new StringBuilder();
+                List<string> vetrai = new List<string>() ;
+                List<string> vephai = new List<string>();
+                int flag = 0, flagvetrai = 1;
+                string output = string.Empty;
+                output += equation + "\r\n";
+                if (!(equation.Contains("khảo sát") || equation.Contains("Khảo sát") || equation.Contains("khao sat") || equation.Contains("khảosát") || equation.Contains("khaosat") || equation.Contains("Khaosat") || equation.Contains("Khao sat")))
+                {
+                    output += "Bài toán khảo sát hàm số: ";
+                    txtResult.Text = output.ToString();
+                   // return;
+                }
+                    
+                equation = equation.Substring(equation.IndexOf(":")+1);
+                //xoa khoang trang
+                equation = equation.Replace(" ", string.Empty);
+                equation = equation.Replace('—', '-');
+                equation = equation.Replace('I', '1');
+                equation = equation.Replace('Z', '2');
+                equation = equation.Replace('S', '5');
+
+                //equation = equation.Replace("\n", string.Empty);
+
+                output += equation + "\r\n";
+                for (int i = 0; i < equation.Length; i++)
+                {
+                    if(equation[i]=='+'|| equation[i] == '-' || equation[i] == '—' || equation[i] == '=' || i+1 == equation.Length)
+                    {
+                        if (builder.ToString() != "" && flagvetrai == 1)
+                        {
+                            vetrai.Add(builder.ToString());
+                        }
+                        if (builder.ToString() != "" && flagvetrai == 0)
+                        {
+                            vephai.Add(builder.ToString());
+                        }
+                        builder.Clear();
+                        if (equation[i] == '=')
+                        {
+                            flagvetrai = 0;
+                            continue;
+                        }
+                            
+                    }
+                    builder.Append(equation[i]);
+                }
+                builder.Clear();
+                string out2 = String.Empty;
+                int dkX = 0 ,bien3 = 0;
+                int bientam = 0;
+                //Ve trai get bien
+                for (int i = 0; i < vetrai.Count; i++)
+                {
+                    bientam = 0;
+                    dkX = 0;
+                    for (int j = 0; j < vetrai[i].Length; j++)
+                    {
+                        out2 += vetrai[i].ElementAt(j).ToString();
+                        if (vetrai[i].ElementAt(j)=='X' || vetrai[i].ElementAt(j) == 'x' )
+                        {
+                            dkX = 1;
+                            if (builder.ToString() == ""|| builder.ToString() == "-" || builder.ToString() == "\n")
+                                builder.Append(1);
+                            if(j+1< vetrai[i].Length)
+                            {
+                                
+                                if (vetrai[i].ElementAt(j + 1) == '2' || vetrai[i].ElementAt(j + 1) =='²')
+                                {
+                                    Int32.TryParse(builder.ToString(), out bientam);
+                                    bien_a += bientam;
+                                    break;
+                                }
+                                if (vetrai[i].ElementAt(j + 1) == '3')
+                                {
+                                    Int32.TryParse(builder.ToString(), out bientam);
+                                    bien3 += bientam;
+                                    break;
+                                }
+                            }
+                            
+                            Int32.TryParse(builder.ToString(), out bientam);
+                            bien_b += bientam;
+                            break;
+
+                        }
+                        builder.Append(vetrai[i].ElementAt(j));
+                    }
+                    if (dkX == 0)
+                    {
+                        Int32.TryParse(builder.ToString(), out bientam);
+                        bien_c += bientam;
+                    }
+                    builder.Clear();
+                }
+                //Ve phai get bien
+                for (int i = 0; i < vephai.Count; i++)
+                {
+                    bientam = 0;
+                    dkX = 0;
+                    for (int j = 0; j < vephai[i].Length; j++)
+                    {
+                        out2 += vephai[i].ElementAt(j).ToString();
+                        if (vephai[i].ElementAt(j) == 'X' || vephai[i].ElementAt(j) == 'x')
+                        {
+                            dkX = 1;
+                            if (builder.ToString() == "" || builder.ToString() == "-" || builder.ToString() == "\n")
+                                builder.Append(1);
+                            if (j + 1 < vephai[i].Length)
+                            {
+
+                                if (vephai[i].ElementAt(j + 1) == '2'|| vephai[i].ElementAt(j + 1) == '²')
+                                {
+                                    Int32.TryParse(builder.ToString(), out bientam);
+                                    bien_a -= bientam;
+                                    break;
+                                }
+                                if (vephai[i].ElementAt(j + 1) == '3')
+                                {
+                                    Int32.TryParse(builder.ToString(), out bientam);
+                                    bien3 -= bientam;
+                                    break;
+                                }
+                            }
+
+                            Int32.TryParse(builder.ToString(), out bientam);
+                            bien_b -= bientam;
+                            break;
+
+                        }
+                        builder.Append(vephai[i].ElementAt(j));
+                    }
+                    if (dkX == 0)
+                    {
+                        Int32.TryParse(builder.ToString(), out bientam);
+                        bien_c -= bientam;
+                    }
+                    builder.Clear();
+                }
+
+                //Xuat du lieu test thu
+                int sizeOfList = vetrai.Count;
+                output += "Ve trai :\r\n";
+                for (int i = 0; i < vetrai.Count; i++)
+                {
+                    output += vetrai[i]+ "\r\n";
+                }
+                output += "Ve phai :\r\n";
+                for (int i = 0; i < vephai.Count; i++)
+                {
+                    output += vephai[i] + "\r\n";
+                }
+                output += "\r\nbien_c: " +bien_c+ "\r\nbien_b: " + bien_b+ "\r\nbien_a: " + bien_a+ "\r\nBien3: " + bien3;
+                txtResult.Text = output;
+                txtInput.Text = bien_a + "," + bien_b + "," + bien_c;
+
+
+                //MapleEngine.MapleCallbacks cb;
+                //byte[] err = new byte[2048];
+                //IntPtr kv;
+
+                //String[] argv = new String[2];
+                //argv[0] = "maple";
+                //argv[1] = "-A2";
+
+                //cb.textCallBack = cbText;
+                //cb.errorCallBack = cbError;
+                //cb.statusCallBack = cbStatus;
+                //cb.readlineCallBack = null;
+                //cb.redirectCallBack = null;
+                //cb.streamCallBack = null;
+                //cb.queryInterrupt = null;
+                //cb.callbackCallBack = null;
+                //Rich.Text = "";
+                //str = "";
+
+
+                //int a = 2, b = 4, c = 6;
+
+                //try
+                //{
+
+                //    kv = MapleEngine.StartMaple(2, argv, ref cb, IntPtr.Zero, IntPtr.Zero, err);
+
+                //    try
+                //    {
+                //        //sử dụng hàm Tong trong package TEST
+                //        String expr = "Tong(";
+
+                //        expr += txtInput.Text;
+                //        //expr += a + "," + b + "," + c;
+                //        expr += ");";
+
+                       
+
+                //       // if (callbackDelegate != null) throw new InvalidOperationException("Can't hook more than once");
+                //        //IntPtr hInstance = LoadLibrary("User32"); its: kv
+                //        //callbackDelegate = new MapleEngine.TextCallBack(cbText);
+                //        //hhook = SetWindowsHookEx(WH_KEYBOARD_LL, callbackDelegate, hInstance, 0);
+                //        IntPtr val = MapleEngine.EvalMapleStatement(kv, Encoding.ASCII.GetBytes(expr));
+                //        //if (val == IntPtr.Zero) throw new Win32Exception();
+                //    }
+                //    catch (Exception)
+                //    {
+                //        MessageBox.Show("Không thể load Maple", "Lỗi", MessageBoxButtons.OK);
+                //    }
+                //}
+                //catch (Exception)
+                //{
+                //}
+                //string tmp = "D:\\TAM\\";
+                //// txtOutput.Text = Path.Combine(tmp, "Eq2ImgWinForms.gif");
+                //txtOutput.Text = str;
+                //NativeMethods.CreateGifFromEq(str, Path.Combine(tmp, "Eq2ImgWinForms.gif"));
+                //pictureBox1.Image = Image.FromFile(Path.Combine(tmp, "Eq2ImgWinForms.gif"));
+                //pictureBox2.Image = Image.FromFile(Path.Combine(tmp, "plot.jpg"));
+
+
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    
+    }
+
+}
+
+       
+       
+
+
+
+  
